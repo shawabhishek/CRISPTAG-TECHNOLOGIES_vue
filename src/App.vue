fe9e328db361c172ffea39158b69e4ef794f0374
abhishek <shaw.rockshek66@gmail.com>
2020-09-11 12:38:19 +0530

    final

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <UpperCase/>
+</div>
+</template>
+
+<script>
+import UpperCase from './components/uppercase'
+export default {
+  name: 'App',
+  components: {
+    UpperCase
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=BioRhyme:wght@700&display=swap');
+  *{
+  width:300px;
+	padding:20px;
+  padding-right: 10px;
+	margin:10px;
+	background: #FFF;
+	border-radius: 10px;
+  }
+  body{
+    font-family: 'BioRhyme', serif;
+    line-height: 1.4;
+  }
+</style>
---
+++ src/components/uppercase.vue
@@ -0,0 +1,21 @@
+  
+<template>
+  <div>
+    <h2>Enter Text</h2>
+    <input v-model="message" placeholder="enter text" />
+    <p>Converted text: {{ message.toUpperCase() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "",
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
